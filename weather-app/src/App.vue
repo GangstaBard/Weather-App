c9655dc62ba58896c3ae485f78054ad244681ae6
Aleospen <tsarexecutor@gmail.com>
2023-04-09 19:18:22 +0300

    first commit

---
+++ weather-app/src/App.vue
@@ -0,0 +1,156 @@
+<template>
+  <main>
+    <div class="search-box">
+      <input
+        v-model="query"
+        @keypress="fetchWeather"
+        type="text"
+        class="search-bar"
+        placeholder="Seacrh..."
+      />
+    </div>
+
+    <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+      <div class="location-box">
+        <div class="location">
+          {{ weather.name }}, {{ weather.sys.country }}
+        </div>
+      </div>
+
+      <div class="weather-box">
+        <div class="temp">{{ Math.round(weather.main.temp) }}°</div>
+        <div class="weather">{{ weather.weather[0].main }}</div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      api_key: "2b7e4ee2fbe82e23e02685340a96e3f8",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+    };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
+            return res.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Montserrat", sans-serif;
+}
+
+#app {
+  background-image: url("@/assets/cold-bg.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0 16px 0 16px;
+  transition: 0.3s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0 16px 0;
+}
+
+.location-box .location {
+  padding-top: 1em;
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box {
+  padding-top: 0.5em;
+  color: #fff;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+}
+
+.weather-box {
+  padding-top: 0.5em;
+  text-align: center;
+}
+
+.weather-box .temp {
+  color: #fff;
+  display: inline-block;
+  text-align: center;
+  padding: 10px 25px;
+  font-size: 100px;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #fff;
+  text-align: center;
+  font-size: 48px;
+  font-weight: 500;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+</style>
